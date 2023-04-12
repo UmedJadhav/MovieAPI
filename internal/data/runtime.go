39ca6d9de63cd032ba2a7e0db5d941debc726dfa
Umed Jadhav <umed.jadhav@phonepe.com>
2023-04-12 22:45:06 +0530

    [chore] implemented custom marshal json

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -11,4 +15,27 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJSONVal := strconv.Quote(jsonValue)
 	return []byte(quotedJSONVal), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONVal, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONVal, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Convert the int32 to a runtime type and assign this to reciever.
+	// using * operator to derefernce the reciever in order to set the underlying value of pointer.
+	*r = Runtime(i)
+	return nil
 }
