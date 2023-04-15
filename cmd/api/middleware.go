dc90a41be23cf10f2fa71189cc1ce97d85b10769
Umed Jadhav <umedj.formal@gmail.com>
2023-04-15 14:24:07 +0530

    [impl] added IP based rate limiter

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
@@ -21,14 +24,53 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	reqPerSec := 2
-	maxReqBurst := 4
-	limiter := rate.NewLimiter(rate.Limit(reqPerSec), maxReqBurst)
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	// Launch a background gorutine which removes old entries from the clinet map once every minute
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			mu.Lock()
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
+
 	h := func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow() {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		mu.Lock()
+		if _, found := clients[ip]; !found {
+			reqPerSec := 2
+			maxReqBurst := 4
+			clients[ip] = &client{limiter: rate.NewLimiter(rate.Limit(reqPerSec), maxReqBurst)}
+		}
+
+		clients[ip].lastSeen = time.Now()
+
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		mu.Unlock()
 		next.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(h)
